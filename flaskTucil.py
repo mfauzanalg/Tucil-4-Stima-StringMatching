1e58f3c1e20d96d7e7d6154abc18fcaa46e55a7a
mfauzanalg <mfauzanalg@gmail.com>
2020-04-22 08:22:22 +0700

    kalo gadapet

--- flaskTucil.py
+++ flaskTucil.py
@@ -14,12 +14,18 @@ def home():
     else:
         return render_template("index.html")
 
-@app.route("/result/<patt>")
+@app.route("/result/<patt>", methods =["POST", "GET"])
 def result(patt):
-    P = "terkonfirmasi positif"
-    hasil = process("Corona Satu.txt", patt)
-    hasil += process("Corona Dua.txt", patt)
-    return render_template("index.html", list=hasil)
+    if request.method == "POST":
+        pattern = request.form["key"]
+        return redirect(url_for("result", patt=pattern))
+    else:
+        hasil = process("Corona Satu.txt", patt)
+        hasil += process("Corona Dua.txt", patt)
+        if not(hasil):
+            return render_template("index.html", list=[["tidak ditemukan", "tidak ditemukan", "tidak ditemukan", "tidak ditemukan"]])
+        else:
+            return render_template("index.html", list=hasil)
 
 
 if __name__ == "__main__":
